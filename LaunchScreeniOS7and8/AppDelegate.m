3706580c4dc0d5ee29eab7f2d3d459f1710769b6
Marshal Wu <marshal@MarshalsMBP.local>
2014-09-17 11:19:07 +0800

    add code by master

--- LaunchScreeniOS7and8/AppDelegate.m
+++ LaunchScreeniOS7and8/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"modified by master");
     return YES;
 }
 
